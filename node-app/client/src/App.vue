df28e8b19b9d0aac1757ea446da7dc05b1d7eeec
wovert <67668283@qq.com>
2019-06-20 08:51:58 +0800

    add component

--- node-app/client/src/App.vue
+++ node-app/client/src/App.vue
@@ -1,31 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+import jwtDecode from 'jwt-decode'
+export default {
+  name: 'app',
+  created () {
+    if (localStorage.eleToken) {
+      const decode = jwtDecode(localStorage.eleToken)
+      this.$store.dispatch('setIsAutnenticated', !this.isEmpty(decode))
+      this.$store.dispatch('setUser', decode)
+    }
+  },
+  methods: {
+    isEmpty (value) {
+      return (
+        value === undefined ||
+        value === null ||
+        (typeof value === 'object' && Object.keys(value).length === 0) ||
+        (typeof value === 'string' && value.trim().length === 0)
+      )
+    }
+  }
+}
+</script>
+
 <style>
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  width: 100%;
+  height: 100%;
 }
 </style>
--- node-app/client/src/router.js
+++ node-app/client/src/router.js
@@ -1,25 +1,54 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Index from './views/Index.vue'
+import Register from './views/Register'
+import Nofind from './views/404'
+import Login from './views/Login'
+import Home from './views/Home'
+import InfoShow from './views/InfoShow'
+import FoundList from './views/FoundList'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    { path: '*', name: '/404', component: Nofind },
+    { path: '/', redirect: '/index' },
+    { path: '/register', name: 'register', component: Register },
+    { path: '/login', name: 'login', component: Login },
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/index',
+      name: 'index',
+      component: Index,
+      children: [
+        { path: '', component: Home },
+        { path: '/home', name: 'home', component: Home },
+        { path: '/infoshow', name: 'infoshow', component: InfoShow },
+        { path: '/foundlist', name: 'foundlist', component: FoundList }
+      ]
     }
+
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: 'about' */ './views/About.vue')
+    // }
   ]
 })
+
+// 添加路由守卫
+router.beforeEach((to, from, next) => {
+  const isLogin = !!localStorage.eleToken
+  if (to.path === '/login' || to.path === '/register') {
+    next()
+  } else {
+    isLogin ? next() : next('/login')
+  }
+})
+
+export default router
